4747f3ea0b3c13e2cefa17c9a6c494abede586c6
Souheib Ben Mabrouk <souheib.benmabrouk@supcom.tn>
2024-01-03 08:34:07 +0100

    add metropolis-within-gibbs-sampler

--- sampler/sampler.py
+++ sampler/sampler.py
@@ -2,13 +2,14 @@ import torch
 import torch.nn.functional as F
 from torch.distributions import MultivariateNormal
 from sklearn.metrics import f1_score
-
+from variational_ae import loss_vae
 class Sampler:
 
-    def __init__(self, vae_model, data, noisy_data, missing_data_indices, T, sampling_algorithm='pseudo_gibbs'):
+    def __init__(self, vae_model, data, noisy_data, missing_data_indices, T, sampling_algorithm='pseudo_gibbs',input_size=28):
 
         self.vae_model = vae_model
         self.data = data
+        self.input_size = input_size
         self.noisy_data = noisy_data
         self.missing_data_indices = missing_data_indices
         self.T = T
@@ -41,11 +42,48 @@ class Sampler:
             binary_data[binary_data >= 0.5] = 1
             binary_data[binary_data < 0.5] = 0
             self.f1_score.append(f1_score(x,binary_data))
-            x = x.view(1, 1, 28, 28)
+            x = x.view(1, 1, self.input_size, self.input_size)
             self.samples.append(x.data.numpy().copy())
             
 
-    def sample_metropolis_within_gibbs(self):
-        # TO DO
-        return
+    def sample_metropolis_within_gibbs(self): # à revoir
+        x = self.noisy_data.clone()
+        binary_data = self.data.clone().view(-1)
+        z_current = torch.rand_like((self.vae_model.encode(x)))
+        for _ in range(1,self.T+1):
+            z = self.metropolis_within_gibbs_step(x,z_current)
+            estimated_x = self.vae_model.decode(z).view(-1)
+            x = x.view(-1)
+            x[self.missing_data_indices] = estimated_x[self.missing_data_indices]
+            x[x < 0.5] = 0
+            x[x >= 0.5] = 1
+            binary_data[binary_data >= 0.5] = 1
+            binary_data[binary_data < 0.5] = 0
+            self.f1_score.append(f1_score(x,binary_data))
+            x = x.view(1, 1, self.input_size, self.input_size)
+            self.samples.append(x.data.numpy().copy())
+            z_current = z
+
+
+    def metropolis_within_gibbs_step(self, current_x,z_current):
+        current_z = z_current
+        proposed_z = self.vae_model.encode(current_x)
+        current_log_joint_prob = self.log_prob(current_x, current_z)
+        proposed_log_joint_prob = self.log_prob(current_x, proposed_z)
+        accept_prob = min(torch.exp(proposed_log_joint_prob - current_log_joint_prob).item(), 1.0)
+
+        if torch.rand(1).item() <= accept_prob:
+            return proposed_z
+        else:
+            return current_z
+
+    def log_prob(self, x, z):
+        decoded_z = self.vae_model.decode(z)
+        z_mean, z_log_var, reconstruction = self.vae_model(decoded_z)
+        return -loss_vae(beta=1.0,
+                                        original=x,
+                                        z_mean=z_mean,
+                                        z_log_var=z_log_var,
+                                        reconstructed=decoded_z)
+
   
--- sampler/test_sampler.py
+++ sampler/test_sampler.py
@@ -8,27 +8,37 @@ from torchvision import datasets, transforms
 from sampler import Sampler
 from variational_ae import VAE
 import seaborn as sns 
+import argparse
 
 sns.set_style("whitegrid")
 np.random.seed(42)
 
-def simulate_missing_data(image,fraction,type):
+def simulate_missing_data(image,fraction,missing_type,input_size=28):
+    """
+    Simulate missing data in an image based on the specified type and fraction.
+
+    Parameters:
+        image (torch.Tensor): Input image.
+        fraction (float): Fraction of missing data to simulate.
+        missing_type (str): Type of missing data ('random', 'half_top', or 'half_bottom').
+        
+    """
     
     imputed_image = image.clone()
     if missing_type == 'random':
-        num_pixels = int(fraction * 28 * 28)
-        pixel_indices = random.sample(range(28 * 28), num_pixels)
+        num_pixels = int(fraction * input_size * input_size)
+        pixel_indices = random.sample(range(input_size * input_size), num_pixels)
         imputed_image = imputed_image.view(-1)
         imputed_image[pixel_indices] = torch.rand((num_pixels,))
-        imputed_image = imputed_image.view(1, 1, 28, 28)
+        imputed_image = imputed_image.view(1, 1, input_size, input_size)
   
     elif missing_type == 'half_top':
-        imputed_image[:, :, :14, :] = torch.rand((1, 1, 14, 28))
-        pixel_indices = torch.arange(0, 14 * 28)
+        imputed_image[:, :, :input_size//2, :] = torch.rand((1, 1, input_size//2, input_size))
+        pixel_indices = torch.arange(0, (input_size//2) * input_size)
 
     elif missing_type == 'half_bottom':
-        imputed_image[:, :, 14:, :] = torch.rand((1, 1, 14, 28))
-        pixel_indices = torch.arange(14 * 28, 28 * 28)
+        imputed_image[:, :, input_size//2:, :] = torch.rand((1, 1, input_size//2, input_size))
+        pixel_indices = torch.arange((input_size//2) * input_size, input_size * input_size)
 
     else:
         raise ValueError("Invalid missing data type.")
@@ -36,98 +46,251 @@ def simulate_missing_data(image,fraction,type):
     return imputed_image,pixel_indices
 
     
-# Load the trained VAE model
-vae_model = VAE()  # Replace with your actual trained VAE model
-vae_model.load_state_dict(torch.load('../model/model_weights/VAE_MNIST_zdim_64_epochs_50.pth'))
-vae_model.eval()
+def run_sampling(sampling_algorithm, vae_model, test_dataset, scenarios, save_path="./results/", T=100):
+    """
+    Run sampling for different missing data scenarios using the specified sampling algorithm.
 
-# Load the MNIST dataset
-transform = transforms.Compose([transforms.ToTensor()])
-test_dataset = datasets.MNIST(root='../mnist_data', 
-                                train=False, 
-                                transform=transform, 
-                                download=True)
-    
-# Define the missing data scenarios
-missing_data_scenarios = [
-    {'fraction': 0.4, 'type': 'random'},
-    {'fraction': 0.5, 'type': 'random'},
-    {'fraction': 0.6, 'type': 'random'},
-    {'fraction': 0.7, 'type': 'random'},
-    {'fraction': 0.8, 'type': 'random'},
-    {'fraction': 0.5, 'type': 'half_top'},
-    {'fraction': 0.5, 'type': 'half_bottom'},
-]
+    Parameters:
+        sampling_algorithm (str): Sampling algorithm ('pseudo_gibbs' or 'metropolis_within_gibbs').
+        vae_model (VAE): Trained Variational Autoencoder model.
+        test_dataset: MNIST test dataset.
+        save_path (str): Path to save the results.
+        T (int): Number of sampling iterations.
+    """
 
-save_path ="./results/"
-sampling_algorithm='pseudo_gibbs'
-T=100
-# Iterate over missing data scenarios
-for scenario in missing_data_scenarios:
-    fraction = scenario['fraction']
-    missing_type = scenario['type']
+    for scenario in scenarios:
+        fraction = scenario['fraction']
+        missing_type = scenario['type']
 
-    print (f"Senario {sampling_algorithm}: \n   fraction : {fraction} \n    type:{missing_type} ")
+        print(f"Scenario {sampling_algorithm}: \n   Fraction: {fraction} \n   Type: {missing_type} ")
 
-    # Generate the missing data scenario
-    original_images = []
-    binary_images = []
-    noisy_images = []
-    imputed_images = []
-    f1_scores = []
+        # Generate the missing data scenario
+        binary_images = []
+        noisy_images = []
+        imputed_images = []
+        f1_scores = []
 
-    for i in range(25):
-        image, label = test_dataset[i]
-        image = image.unsqueeze(0)
-                
-        noisy_image,indices_to_remove = simulate_missing_data(image,fraction,missing_type)
-        # Convert to binary
-        binary_image = (image >= 0.5).float()
+        for i in range(5):
+            image, _ = test_dataset[i]
+            image = image.unsqueeze(0)
 
-        # Create VaeSampler instance
-        sampler = Sampler(vae_model, 
-                          image, 
-                          noisy_image, 
-                          indices_to_remove, 
-                          T=T,
-                          sampling_algorithm=sampling_algorithm)
+            noisy_image, indices_to_remove = simulate_missing_data(image, fraction, missing_type)
 
-        # Perform sampling
-        imputed_image, f1_score_values = sampler.sample()
+            # Convert to binary
+            binary_image = (image >= 0.5).float()
 
-        # Append to lists
-        #original_images.append(image.squeeze().numpy())
-        binary_images.append(binary_image.squeeze().numpy())
-        noisy_images.append(noisy_image.squeeze().numpy())
-        imputed_images.append(imputed_image[-1].squeeze())
-        f1_scores.append(f1_score_values[-1])
-        
-    # Plot F1-score evolution for 1 image
-    plt.figure(figsize=(10,5))
-    plt.plot(range(1,T+1),f1_score_values)
-    plt.xlabel("sampling iteration")
-    plt.ylabel("F1-score")
-    plt.title(f"Pseudo_Gibbs: Missing {fraction*100}% {missing_type}")
-    plt.savefig(save_path+sampling_algorithm+"_F1evolution_"+str(fraction*100)+"_"+missing_type+".PNG")
-    # Plot the images
-    fig, axs = plt.subplots(3, 5, figsize=(20, 10))
+            # Create VaeSampler instance
+            sampler = Sampler(vae_model, image, noisy_image, indices_to_remove, T=T, sampling_algorithm=sampling_algorithm)
 
-    for j in range(5):
-        # Plot binary image
-        axs[0, j].imshow(binary_images[j], cmap='gray')
-        axs[0, j].axis('off')
-        axs[0, j].set_title('Binary original')
+            # Perform sampling
+            imputed_image, f1_score_values = sampler.sample()
 
-        # Plot missing image
-        axs[1, j].imshow(noisy_images[j], cmap='gray')
-        axs[1, j].axis('off')
-        axs[1, j].set_title(f'Missing {fraction*100}% {missing_type}')
+            # Append to lists
+            binary_images.append(binary_image.squeeze().numpy())
+            noisy_images.append(noisy_image.squeeze().numpy())
+            imputed_images.append(imputed_image[-1].squeeze())
+            f1_scores.append(f1_score_values[-1])
 
-        # Plot reconstructed image
-        axs[2, j].imshow(imputed_images[j], cmap='gray')
-        axs[2, j].axis('off')
-        axs[2, j].set_title(f'Reconstructed f1={f1_scores[j]:.2f}')
+        # Plot F1-score evolution for 1 image
+        plt.figure(figsize=(10, 5))
+        plt.plot(range(1, T + 1), f1_score_values)
+        plt.xlabel("Sampling Iteration")
+        plt.ylabel("F1-score")
+        plt.title(f"{sampling_algorithm.capitalize()}: Missing {fraction * 100}% {missing_type}")
+        plt.savefig(save_path +"/"+ sampling_algorithm + "/"+sampling_algorithm + "_F1evolution_" + str(fraction * 100) + "_" + missing_type + ".PNG")
+
+        # Plot the images
+        fig, axs = plt.subplots(3, 5, figsize=(20, 10))
+
+        for j in range(5):
+            # Plot binary image
+            axs[0, j].imshow(binary_images[j], cmap='gray')
+            axs[0, j].axis('off')
+            axs[0, j].set_title('Binary original')
+
+            # Plot missing image
+            axs[1, j].imshow(noisy_images[j], cmap='gray')
+            axs[1, j].axis('off')
+            axs[1, j].set_title(f'Missing {fraction * 100}% {missing_type}')
+
+            # Plot reconstructed image
+            axs[2, j].imshow(imputed_images[j], cmap='gray')
+            axs[2, j].axis('off')
+            axs[2, j].set_title(f'Reconstructed f1={f1_scores[j]:.2f}')
 
         plt.tight_layout()
-        plt.savefig(save_path+sampling_algorithm+"_"+str(int(fraction*100))+"_"+missing_type+".PNG")
-    print(f"average f1_score {np.mean(f1_scores):.2f}" )
+        plt.savefig(save_path +"/"+ sampling_algorithm + "/"+ sampling_algorithm + "_" + str(int(fraction * 100)) + "_" + missing_type + ".PNG")
+
+        print(f"    Average F1-score: {np.mean(f1_scores):.2f}")
+        
+
+def compare_sampling_algorithms(vae_model, test_dataset, scenarios, save_path="./results/", T=100,input_size=28):
+    """
+    Compare Pseudo-Gibbs and Metropolis-Within-Gibbs sampling algorithms for various missing data scenarios.
+
+    Parameters:
+        vae_model (VAE): Trained Variational Autoencoder model.
+        test_dataset: test dataset.
+        scenarios (list): List of dictionaries specifying missing data scenarios.
+        save_path (str): Path to save the results.
+        T (int): Number of sampling iterations.
+    """
+    for scenario in scenarios:
+        fraction = scenario['fraction']
+        missing_type = scenario['type']
+
+        print(f"Scenario: \n   Fraction: {fraction} \n   Type: {missing_type}")
+
+        binary_images = []
+        noisy_images = []
+        imputed_images_pg = []
+        imputed_images_mwg = []
+        f1_scores_pg = []
+        f1_scores_mwg = []
+
+        for i in range(1,6):
+            image, _ = test_dataset[i]
+            image = image.unsqueeze(0)
+
+            noisy_image, indices_to_remove = simulate_missing_data(image, fraction, missing_type,input_size=input_size)
+
+            # Convert to binary
+            binary_image = (image >= 0.5).float()
+
+            # Create VaeSampler instance
+            sampler_pg = Sampler(vae_model, 
+                                 image, 
+                                 noisy_image, 
+                                 indices_to_remove, 
+                                 T=T, 
+                                 sampling_algorithm='pseudo_gibbs',
+                                 input_size=input_size)
+            
+            sampler_mwg = Sampler(vae_model, 
+                                  image, 
+                                  noisy_image, 
+                                  indices_to_remove, 
+                                  T=T, 
+                                  sampling_algorithm='metropolis_within_gibbs',
+                                  input_size=input_size)
+
+            # Perform sampling
+            imputed_image_pg, f1_score_values_pg = sampler_pg.sample()
+            imputed_image_mwg, f1_score_values_mwg = sampler_mwg.sample()
+
+            # Append to lists
+            binary_images.append(binary_image.squeeze().numpy())
+            noisy_images.append(noisy_image.squeeze().numpy())
+            imputed_images_pg.append(imputed_image_pg[-1].squeeze())
+            imputed_images_mwg.append(imputed_image_mwg[-1].squeeze())
+            f1_scores_pg.append(f1_score_values_pg[-1])
+            f1_scores_mwg.append(f1_score_values_mwg[-1])
+
+        # Plot F1-score evolution for 1 image
+        plt.figure(figsize=(10, 5))
+        plt.plot(range(1, T + 1), f1_score_values_pg, label="Pseudo-gibbs", linestyle="--")
+        plt.plot(range(1, T + 1), f1_score_values_mwg, label="Metroplis-Within-Gibbs", linestyle="-.")
+        plt.xlabel("Sampling Iteration")
+        plt.ylabel("F1-score")
+        plt.title(f"Comparison: Missing {fraction * 100}% {missing_type}")
+        plt.legend()
+        plt.savefig(save_path + "comparison_F1evolution_" + str(fraction * 100) + "_" + missing_type + ".PNG")
+
+        # Plot the images
+        fig, axs = plt.subplots(4, 5, figsize=(20, 10))
+
+        for j in range(5):
+            axs[0, j].imshow(binary_images[j], cmap='gray')
+            axs[0, j].axis('off')
+            axs[0, j].set_title('Binary original')
+
+            axs[1, j].imshow(noisy_images[j], cmap='gray')
+            axs[1, j].axis('off')
+            axs[1, j].set_title(f'Missing {fraction * 100}% {missing_type}')
+
+            axs[2, j].imshow(imputed_images_pg[j], cmap='gray')
+            axs[2, j].axis('off')
+            axs[2, j].set_title(f'{"Pseudo-gibbs".capitalize()} Reconstruction f1={f1_scores_pg[j]:.2f}')
+            
+            axs[3, j].imshow(imputed_images_mwg[j], cmap='gray')
+            axs[3, j].axis('off')
+            axs[3, j].set_title(f'{"Metroplis-Within-Gibbs".capitalize()} Reconstruction f1={f1_scores_mwg[j]:.2f}')
+
+        plt.tight_layout()
+        plt.savefig(save_path + "comparison_" + str(int(fraction * 100)) + "_" + missing_type + ".PNG")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Inference parameters')
+    parser.add_argument('--dataset', 
+                    type=str, 
+                    default="MNIST", 
+                    help='Dataset for the training')
+    args = parser.parse_args()
+    dataset = args.dataset
+    
+    
+    vae_model = VAE()  # Replace with your actual trained VAE model
+
+    if dataset.upper() == "MNIST" :
+        transform = transforms.Compose([transforms.ToTensor()])
+        test_dataset = datasets.MNIST(root='../datasets/mnist_data', 
+                                        train=False, 
+                                        transform=transform, 
+                                        download=True)
+        vae_model.load_state_dict(torch.load('../model/model_weights/VAE_MNIST_zdim_16_epochs_50.pth'))
+    
+    elif dataset.upper() == "OMNIGLOT" : 
+            transform = transforms.Compose([transforms.Resize((28, 28)),
+                                            transforms.ToTensor()])
+            omniglot_dataset = torchvision.datasets.Omniglot(root='../datasets/omniglot_data',
+                                                  background=True,
+                                                  transform=transform,
+                                                  download=True)
+
+            # Split the dataset into training and testing sets
+            train_size = int(0.7 * len(omniglot_dataset))
+            test_size = len(omniglot_dataset) - train_size
+            train_dataset, test_dataset = torch.utils.data.random_split(omniglot_dataset, [train_size, test_size])
+
+            # Define batch size
+            batch_size = 128
+            test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                                    batch_size=batch_size,
+                                                    shuffle=False)
+            vae_model.load_state_dict(torch.load('../model/model_weights/VAE_OMNIGLOT_zdim_16_epochs_55.pth'))
+    
+    vae_model.eval()
+        
+    # Define the missing data scenarios
+    missing_data_scenarios = [
+        {'fraction': 0.4, 'type': 'random'},
+        {'fraction': 0.5, 'type': 'random'},
+        {'fraction': 0.6, 'type': 'random'},
+        {'fraction': 0.7, 'type': 'random'},
+        {'fraction': 0.8, 'type': 'random'},
+        {'fraction': 0.5, 'type': 'half_top'},
+        {'fraction': 0.5, 'type': 'half_bottom'},
+    ]
+
+    print("run_sampling with pseudo_gibbs")
+    run_sampling(sampling_algorithm='pseudo_gibbs',
+                vae_model=vae_model,
+                test_dataset=test_dataset,
+                scenarios=missing_data_scenarios,
+                T=500)
+
+    print("run_sampling with metropolis_within_gibbs")
+    run_sampling(sampling_algorithm='metropolis_within_gibbs',
+                vae_model=vae_model,
+                test_dataset=test_dataset,
+                scenarios=missing_data_scenarios,
+                T=500)
+
+    print("Compare the methods")
+    compare_sampling_algorithms(vae_model=vae_model, 
+                                test_dataset=test_dataset, 
+                                scenarios=missing_data_scenarios, 
+                                save_path="./results/", 
+                                T=500)
